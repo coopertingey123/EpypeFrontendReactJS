2953bd4353c9b8f41a409e1ab1004013684daa96
Cooper Tingey <cooper.tingey@gmail.com>
2021-08-23 09:47:13 -0700

    admin-homepage complete again

--- src/components/admin/admin-homepage.js
+++ src/components/admin/admin-homepage.js
@@ -1,41 +1,34 @@
-import React from 'react';
-import { Container, ListGroup, Row, Col } from 'react-bootstrap';
-
-import AdminNavbar from "../navigation/navbar-admin";
-import AdminFooter from "../navigation/footer-admin";
+import React from 'react'
+import { Container, ListGroup } from "react-bootstrap"
 
 export default function AdminHomepage(props) {
     return (
-        <div className="page-wrapper">
-            <AdminNavbar/>
-            <Container className="p-5">
-                <h1 className="green-text text-center p-3">General Admin</h1>
-
-                <ListGroup defaultActiveKey="#link1">
-                    <ListGroup.Item action href="">
-                        Manage Accounts
-                    </ListGroup.Item>
-                    <ListGroup.Item action href="" >
-                        Manage Admin Users
-                    </ListGroup.Item>
-                    <ListGroup.Item action>
-                        Manage Promo Codes
-                    </ListGroup.Item>
-                    <ListGroup.Item action href="">
-                        Manage SMS Codes
-                    </ListGroup.Item>
-                    <ListGroup.Item action href="#link2">
-                        Manage Video Landing Page Themes
-                    </ListGroup.Item>
-                    <ListGroup.Item action>
-                        Manage Events
-                    </ListGroup.Item>
-                    <ListGroup.Item action>
-                        Manage Email Notices
-                    </ListGroup.Item>
-                </ListGroup>
-            </Container>
-            <AdminFooter/>
-        </div>
+        
+        <Container>
+            <h1 className="green-text m-3">General Admin</h1>
+            <ListGroup className="m-3">
+                <ListGroup.Item action href="">
+                    Manage Accounts
+                </ListGroup.Item>
+                <ListGroup.Item action href="">
+                    Manage Admin Users
+                </ListGroup.Item>
+                <ListGroup.Item action href="">
+                    Manage Promo Codes
+                </ListGroup.Item>
+                <ListGroup.Item action href="">
+                    Manage SMS Codes
+                </ListGroup.Item>
+                <ListGroup.Item action href="">
+                    Manage Video Landing Page Themes
+                </ListGroup.Item>
+                <ListGroup.Item action href="">
+                    Manage Events
+                </ListGroup.Item>
+                <ListGroup.Item action href="">
+                    Manage Email Notices
+                </ListGroup.Item>
+            </ListGroup>
+        </Container>
     )
 }
