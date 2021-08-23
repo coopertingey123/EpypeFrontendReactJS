3df8b6bd8b636597e9e9bb01627797597703c52b
Cooper Tingey <cooper.tingey@gmail.com>
2021-08-23 12:22:12 -0700

    added admin users component

--- src/components/admin/admin-homepage.js
+++ src/components/admin/admin-homepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container, ListGroup } from "react-bootstrap"
 
 import ManageAccounts from "./manage-accounts"
+import AdminUsers from "./admin-users"
 
 export default function AdminHomepage(props) {
     return (
@@ -32,6 +33,7 @@ export default function AdminHomepage(props) {
                 </ListGroup.Item>
             </ListGroup>
             <ManageAccounts/>
+            <AdminUsers/>
         </Container>
     )
 }
---
+++ src/components/admin/admin-users.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Container, Form, Button, Row, Col } from 'react-bootstrap'
+
+export default function (props) {
+    return (
+        <Container className="text-center">
+            <h1 className="green-text p-3">
+                Admin Users
+            </h1>
+            <Button>+ Add a new Admin User</Button>
+            <Form className="text-start p-3">
+                <Form.Group className="p-3">
+                    <Form.Label>
+                        Search
+                    </Form.Label>
+                    <select className="form-select my-2" aria-label="All Fields">
+                        <option value="">All Fields</option>
+                        <option value="">User ID</option>
+                        <option value="">Name</option>
+                        <option value="">Email Address</option>
+                    </select>
+                    <Form.Control/>
+                </Form.Group>
+                <Form.Group className="p-3">
+                    <Form.Label>
+                        Admin User Type
+                    </Form.Label>
+                    <select className="form-select my-2" aria-label="All Types">
+                        <option value="">All Types</option>
+                        <option value="">Accounting</option>
+                        <option value="">Support</option>
+                        <option value="">Sales</option>
+                    </select>
+                </Form.Group>
+                <Form.Group className="p-3">
+                    <Form.Label>
+                        Active
+                    </Form.Label>
+                    <select className="form-select my-2" aria-label="Doesn't Matter">
+                        <option value="">Yes</option>
+                        <option value="">No</option>
+                    </select>
+                </Form.Group>
+                <Form.Group className="p-3">
+                    <Form.Label>
+                        Sort Results By
+                    </Form.Label>
+                    <Row>
+                        <Col>
+                            <select className="form-select" aria-label="Last Name">
+                                <option value="" selected>User Id</option>
+                                <option value="" selected>Last Name</option>
+                                <option value="">First Name</option>
+                                <option value="">Email Address</option>
+                            </select>
+                        </Col>
+                        <Col>
+                            <select className="form-select" aria-label="Ascending">
+                                <option value="" selected>Descending</option>
+                                <option value="">Ascending</option>
+                            </select>
+                        </Col>
+                    </Row>
+                </Form.Group>
+            </Form>
+            <Button className="m-3">Find Accounts</Button>
+            <Button className="m-3">Previous Search</Button>
+        </Container>
+
+    )
+}
