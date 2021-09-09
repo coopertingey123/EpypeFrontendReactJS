71b79d52ec653f95dc01e41834d43d8ca3a53a6f
Cooper Tingey <cooper.tingey@gmail.com>
2021-09-09 13:51:20 -0700

    auth admin pages updated with state and mobile-responsive

--- src/components/auth/admin/admin-change-password.js
+++ src/components/auth/admin/admin-change-password.js
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
 
 export default function AdminChangePassword(props) {
+
+    const [currentPassword, setCurrentPassword] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [confirmNewPassword, setConfirmNewPassword] = useState("")
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(currentPassword)
+        console.log(newPassword)
+        console.log(confirmNewPassword)
+      }
+
     return (
         <Container className="text-center">
             <h1 className="green-text pt-3">Change Password</h1>
-            <Form className="py-3 text-start">
+            <Form className="py-3 text-start" onSubmit={handleSubmit}>
                 <Form.Group className="py-2">
                     <Form.Label>Current Password</Form.Label>
-                    <Form.Control/>
+                    <Form.Control
+                        value={currentPassword}
+                        onChange={(e) => setCurrentPassword(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group className="py-2">
                     <Form.Label>New Password</Form.Label>
-                    <Form.Control/>
+                    <Form.Control
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        value={newPassword}
+                    />
                 </Form.Group>
                 <Form.Group className="py-2">
                     <Form.Label>Confirm new password</Form.Label>
-                    <Form.Control/>
+                    <Form.Control
+                        onChange={(e) => setConfirmNewPassword(e.target.value)}
+                        value={confirmNewPassword}
+                    />
                 </Form.Group>
+                <div className="text-center">
+                    <Button className="m-1" type="submit">Change Password</Button>
+                    <Button className="btn-danger m-1">Cancel</Button>
+                </div>
             </Form>
-            <Button className="m-1">Change Password</Button>
-            <Button className="btn-danger m-1">Cancel</Button>
+            
         </Container>
     )
 }
--- src/components/auth/admin/admin-preferences.js
+++ src/components/auth/admin/admin-preferences.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
 
 export default function AdminChangePreferences(props) {
+
+    const [timeZone, setTimeZone] = useState("")
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(timeZone)
+    }
+
     return (
         <Container className="text-center">
             <h1 className="green-text pt-3">Change Preferences</h1>
-            <Form className="py-3 text-start">
+            <Form className="py-3 text-start" onSubmit={handleSubmit}>
                 <Form.Group className="py-2">
                     <Form.Label>Time Zone</Form.Label>
-                    <select className="form-select">
-                        <option value="">Mountain</option>
-                        <option value="">Alaska</option>
-                        <option value="">Arizona</option>
-                        <option value="">Central</option>
-                        <option value="">Eastern</option>
-                        <option value="">Hawaii-Aleutian</option>
-                        <option value="">Pacific</option>
+                    <select className="form-select" onChange={(e) => setTimeZone(e.target.value)}>
+                        <option value="mountain">Mountain</option>
+                        <option value="alaska">Alaska</option>
+                        <option value="arizona">Arizona</option>
+                        <option value="central">Central</option>
+                        <option value="eastern">Eastern</option>
+                        <option value="hawaii-aleutian">Hawaii-Aleutian</option>
+                        <option value="pacific">Pacific</option>
                     </select>
                 </Form.Group>
+                <div className="text-center">
+                    <Button className="m-1" type="submit">Save Changes</Button>
+                    <Button className="btn-danger m-1">Cancel</Button>
+                </div>
             </Form>
-            <Button className="m-1">Save Changes</Button>
-            <Button className="btn-danger m-1">Cancel</Button>
         </Container>
     )
 }
