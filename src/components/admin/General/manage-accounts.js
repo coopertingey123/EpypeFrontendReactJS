b8c2750f940c97e3bbe3fe4f15016394f59f4d0f
Cooper Tingey <cooper.tingey@gmail.com>
2021-09-10 14:19:44 -0700

    adjustments to manage-accounts component

--- src/components/admin/General/manage-accounts.js
+++ src/components/admin/General/manage-accounts.js
@@ -1,18 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Form, Button, Col, Row } from 'react-bootstrap'
 
 export default function ManageAccounts(props) {
+
+    const [accountType, setAccountType] = useState("")
+    const [searchField, setSearchField] = useState("")
+    const [search, setSearch] = useState("")
+    // const [includePhone, setIncludePhone] = useState("")
+    const [state, setState] = useState([])
+    const [salesPerson, setSalesPerson] = useState([])
+    const [salesPersonType, setSalesPersonType] = useState("")
+    const [accountStatus, setAccountStatus] = useState("")
+    const [setupStatus, setSetupStatus] = useState("")
+    const [sortResultsOption, setSortResultsOption] = useState("")
+    const [sortOrder, setSortOrder] = useState("")
+    const [orgType, setOrgType] = useState("")
+    // const [includeOrgUserCount, setIncludeOrgUserCount] = useState("")
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(accountType)
+        console.log(searchField)
+        console.log(search)
+        // console.log(includePhone)
+        console.log(state)
+        console.log(salesPerson)
+        console.log(salesPersonType)
+        console.log(accountStatus)
+        console.log(setupStatus)
+        console.log(sortResultsOption)
+        console.log(sortOrder)
+        console.log(orgType)
+        // console.log(includeOrgUserCount)
+      }
+
     return (
         <Container className="text-center">
             <h1 className="green-text p-3">Accounts</h1>
             <Button>+ Add a New Organization</Button>
-            <Form className="text-start">
+            <Form className="text-start" onSubmit={handleSubmit}>
                 <Form.Group className="p-3">
                     <Form.Label>
                         Account Type
                     </Form.Label>
-                    <select className="form-select" aria-label="All">
-                        <option value="" selected>All</option>
+                    <select 
+                        className="form-select" 
+                        aria-label="All"
+                        value={accountType}
+                        onChange={(e) => setAccountType(e.target.value)}
+                    >
+                        <option value="">All</option>
                         <option value="u">Users</option>
                         <option value="o">Organizations</option>
                     </select>
@@ -21,24 +59,39 @@ export default function ManageAccounts(props) {
                     <Form.Label>
                         Search
                     </Form.Label>
-                    <select className="form-select my-2" aria-label="All Fields">
-                        <option value="" selected>All Fields</option>
+                    <select 
+                        className="form-select my-2" 
+                        aria-label="All Fields"
+                        value={searchField}
+                        onChange={(e) => setSearchField(e.target.value)}
+                    >
+                        <option value="">All Fields</option>
                         <option value="u">User ID</option>
                         <option value="o">Name</option>
                         <option value="u">Email Address</option>
                         <option value="o">Phone Number</option>
                     </select>
-                    <Form.Control className="my-2"/>
+                    <Form.Control 
+                        className="my-2"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
                     <Form.Check type="checkbox" label="Don't include phone numbers in 'All Fields'"/>
                 </Form.Group>
                 <Row>
-                    <Col>
+                    <Col lg={6}>
                         <Form.Group className="p-3">
                             <Form.Label>
                                 State
                             </Form.Label>
-                            <select multiple size="7" class="form-select">
-                                <option value="" selected>All States</option>
+                            <select 
+                                // multiple 
+                                size="7" 
+                                className="form-select"
+                                value={state}
+                                onChange={(e) => setState(e.target.value)}
+                            >
+                                <option value="">All States</option>
                                 <option value="AK">AK</option>
                                 <option value="AL">AL</option>
                                 <option value="AR">AR</option>
@@ -96,13 +149,19 @@ export default function ManageAccounts(props) {
                             </select>
                         </Form.Group>
                     </Col>
-                    <Col>
+                    <Col lg={6}>
                         <Form.Group className="p-3">
                             <Form.Label>
                                 Sales Person
                             </Form.Label>
-                            <select multiple size="7" class="form-select">
-					            <option value="" selected>All</option>
+                            <select 
+                                // multiple 
+                                size="7" 
+                                className="form-select"
+                                value={salesPerson}
+                                onChange={(e) => setSalesPerson(e.target.value)}
+                            >
+					            <option value="">All</option>
                                 <option value="0">Unassigned</option>
                                 <option value="9">Ammon Freest0ne (Platpay)</option>
                                 <option value="1">Corporate Sales</option>
@@ -122,8 +181,13 @@ export default function ManageAccounts(props) {
                     <Form.Label>
                         Sales Person
                     </Form.Label>
-                    <select className="form-select" aria-label="All">
-                        <option value="" selected>All</option>
+                    <select 
+                        className="form-select" 
+                        aria-label="All"
+                        value={salesPersonType}
+                        onChange={(e) => setSalesPersonType(e.target.value)}
+                    >
+                        <option value="">All</option>
                         <option value="u">Users</option>
                         <option value="o">Organizations</option>
                     </select>
@@ -132,21 +196,31 @@ export default function ManageAccounts(props) {
                     <Form.Label>
                         Account Status
                     </Form.Label>
-                    <select className="form-select" aria-label="Doesn't Matter">
-                        <option value="" selected>Doesn't Matter</option>
-                        <option value="">Active</option>
-                        <option value="">Suspended</option>
-                        <option value="">Canceled</option>
+                    <select 
+                        className="form-select" 
+                        aria-label="Doesn't Matter"
+                        value={accountStatus}
+                        onChange={(e) => setAccountStatus(e.target.value)}
+                    >
+                        <option value="">Doesn't Matter</option>
+                        <option value="active">Active</option>
+                        <option value="suspended">Suspended</option>
+                        <option value="canceled">Canceled</option>
                     </select>
                 </Form.Group>
                 <Form.Group className="p-3">
                     <Form.Label>
                         Setup Status
                     </Form.Label>
-                    <select className="form-select" aria-label="Doesn't Matter">
-                        <option value="" selected>Doesn't Matter</option>
-                        <option value="">Complete</option>
-                        <option value="">Incomplete</option>
+                    <select 
+                        className="form-select" 
+                        aria-label="Doesn't Matter"
+                        value={setupStatus}
+                        onChange={(e) => setSetupStatus(e.target.value)}
+                    >
+                        <option value="">Doesn't Matter</option>
+                        <option value="complete">Complete</option>
+                        <option value="incomplete">Incomplete</option>
                     </select>
                 </Form.Group>
                 <Form.Group className="p-3">
@@ -154,18 +228,28 @@ export default function ManageAccounts(props) {
                         Sort Results By
                     </Form.Label>
                     <Row>
-                        <Col>
-                            <select className="form-select" aria-label="Last Name">
-                                <option value="" selected>User Id</option>
-                                <option value="" selected>Last Name</option>
-                                <option value="">First Name</option>
-                                <option value="">Email Address</option>
+                        <Col lg={6}>
+                            <select 
+                                className="form-select mb-2" 
+                                aria-label="Last Name"
+                                value={sortResultsOption}
+                                onChange={(e) => setSortResultsOption(e.target.value)}
+                            >
+                                <option value="userId">User Id</option>
+                                <option value="lastName">Last Name</option>
+                                <option value="firstName">First Name</option>
+                                <option value="emailAddress">Email Address</option>
                             </select>
                         </Col>
-                        <Col>
-                            <select className="form-select" aria-label="Ascending">
-                                <option value="" selected>Descending</option>
-                                <option value="">Ascending</option>
+                        <Col lg={6}>
+                            <select 
+                                className="form-select" 
+                                aria-label="Ascending"
+                                value={sortOrder}
+                                onChange={(e) => setSortOrder(e.target.value)}
+                            >
+                                <option value="descending">Descending</option>
+                                <option value="ascending">Ascending</option>
                             </select>
                         </Col>
                     </Row>
@@ -174,30 +258,38 @@ export default function ManageAccounts(props) {
                     <Form.Label>
                         Organization
                     </Form.Label>
-                    <select className="form-select my-2" aria-label="All">
-                        <option value="" selected>All</option>
-                        <option value="">99990 - Individual Agents</option>
-                        <option value="">100004 - American Leadership Academy</option>
-                        <option value="">100012 - Bottega Tech</option>
-                        <option value="">100005 - Demo Account</option>
-                        <option value="">100059 - DLVR - Amazon Partner</option>
-                        <option value="">100115 - doTERRA Brazil</option>
-                        <option value="">100018 - EPYPE Corporation</option>
-                        <option value="">100010 - Global EP</option>
-                        <option value="">100102 - Lifewave</option>
-                        <option value="">100095 - Live Kaya Corporate</option>
-                        <option value="">100024 - NuSkin Enterprises</option>
-                        <option value="">100016 - Platinum Payments</option>
-                        <option value="">99991 - Provo Office</option>
-                        <option value="">100153 - Solle International</option>
-                        <option value="">100014 - The CHurch of Jesus Christ of Latter Day Saints</option>
-                        <option value="">100085 - Vivint</option>
+                    <select 
+                        className="form-select my-2" 
+                        aria-label="All"
+                        value={orgType}
+                        onChange={(e) => setOrgType(e.target.value)}
+                    >
+                        <option value="">All</option>
+                        <option value="123">99990 - Individual Agents</option>
+                        <option value="123">100004 - American Leadership Academy</option>
+                        <option value="123">100012 - Bottega Tech</option>
+                        <option value="123">100005 - Demo Account</option>
+                        <option value="123">100059 - DLVR - Amazon Partner</option>
+                        <option value="123">100115 - doTERRA Brazil</option>
+                        <option value="123">100018 - EPYPE Corporation</option>
+                        <option value="123">100010 - Global EP</option>
+                        <option value="123">100102 - Lifewave</option>
+                        <option value="123">100095 - Live Kaya Corporate</option>
+                        <option value="123">100024 - NuSkin Enterprises</option>
+                        <option value="123">100016 - Platinum Payments</option>
+                        <option value="123">99991 - Provo Office</option>
+                        <option value="123">100153 - Solle International</option>
+                        <option value="123">100014 - The CHurch of Jesus Christ of Latter Day Saints</option>
+                        <option value="123">100085 - Vivint</option>
                     </select>
                     <Form.Check type="checkbox" label="Include Organization User Count"/>
                 </Form.Group>
+                <div className="text-center">
+                    <Button className="m-3" type="submit" href="">Find Accounts</Button>
+                    <Button className="m-3">Previous Search</Button>
+                </div>
             </Form>
-            <Button className="m-3">Find Accounts</Button>
-            <Button className="m-3">Previous Search</Button>
+            
         </Container>
     )
 }
